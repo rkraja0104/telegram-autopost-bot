88b4daa14e596f8493f63ea94df5cf591fd76094
rkraja0104 <rrk00392@gmail.com>
2026-02-27 21:52:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import asyncio
-from pyrogram import Client
-import asyncio
+from telegram import Bot
 
 TOKEN = "8797113384:AAHVgv1dgPY_YUspuCPDT1n-ZihBCAsGRMM"
 
-SOURCE_CHANNEL = -1002839275504
+SOURCE_CHANNEL = -1002839275504  # your source
 DESTINATION_CHANNELS = [
     -1003547480082,
     -1002907412341,
@@ -13,25 +12,28 @@ DESTINATION_CHANNELS = [
     -1003224337028
 ]
 
-bot = Client("my_bot", bot_token=TOKEN)
+bot = Bot(token=TOKEN)
 
-async def forward_posts():
+async def auto_post():
+    last_message_id = 0
+
     while True:
-        async for message in bot.get_chat_history(SOURCE_CHANNEL, limit=20):
-            for channel in DESTINATION_CHANNELS:
-                try:
-                    await bot.forward_message(
-                        chat_id=channel,
-                        from_chat_id=SOURCE_CHANNEL,
-                        message_id=message.message_id
-                    )
-                except Exception as e:
-                    print(e)
+        updates = await bot.get_updates()
 
-        await asyncio.sleep(3600)
+        for update in updates:
+            if update.channel_post and update.channel_post.chat.id == SOURCE_CHANNEL:
+                message_id = update.channel_post.message_id
 
-async def main():
-    async with bot:
-        await forward_posts()
+                if message_id > last_message_id:
+                    for channel in DESTINATION_CHANNELS:
+                        await bot.copy_message(
+                            chat_id=channel,
+                            from_chat_id=SOURCE_CHANNEL,
+                            message_id=message_id
+                        )
 
-asyncio.run(main())
+                    last_message_id = message_id
+
+        await asyncio.sleep(30)
+
+asyncio.run(auto_post())
